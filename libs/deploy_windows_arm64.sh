ac31a66bbbd8f95dba82e1ea21e111838711bc01
Aydin <dklisp@gmail.com>
2025-01-21 18:54:22 +0330

    Create deploy_windows_arm64.sh

---
+++ libs/deploy_windows_arm64.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+source libs/env_deploy.sh
+DEST=$DEPLOYMENT/windows_arm64
+rm -rf $DEST
+mkdir -p $DEST
+
+#### copy exe ####
+cp $BUILD/nekobox.exe $DEST
+
+#### deploy qt & DLL runtime ####
+pushd $DEST
+windeployqt nekobox.exe --no-compiler-runtime --no-system-d3d-compiler --no-opengl-sw --verbose 2 --dir $DEST/arm64
+rm -rf translations
+rm -rf libEGL.dll libGLESv2.dll Qt6Pdf.dll
+
+if [ "$DL_QT_VER" != "5.15" ]; then
+  cp $SRC_ROOT/qtsdk/Qt/bin/libcrypto-3-arm64.dll .
+  cp $SRC_ROOT/qtsdk/Qt/bin/libssl-3-arm64.dll .
+fi
+
+popd
+
+#### prepare deployment ####
+cp $BUILD/*.pdb $DEPLOYMENT
