5c0d7fd07f06a9b06501d323eae45eb9df9be0ec
Aydin <dklisp@gmail.com>
2025-01-21 22:05:48 +0330

    Update download_qtsdk_win.sh make it download x64 version of Qt

--- libs/download_qtsdk_win.sh
+++ libs/download_qtsdk_win.sh
@@ -10,11 +10,13 @@ if [ "$QT_ARCH" == "arm64" ]; then
   if [ "$DL_QT_VER" == "5.15" ]; then
     # Download Qt 5.15 for Windows ARM64
     echo "QTARM64: We're going for the 5.15 version, but we'll download 6.7.2 anyways :)"
-    curl -LSO https://github.com/troffeelituf/nekoray/releases/download/qt-runtime/Qt6.7.2-Windows-arm64-VS2022-17.10.3-20240621.7z
+    # curl -LSO https://github.com/troffeelituf/nekoray/releases/download/qt-runtime/Qt6.7.2-Windows-arm64-VS2022-17.10.3-20240621.7z
+    curl -LSO https://github.com/MatsuriDayo/nekoray_qt_runtime/releases/download/20220503/Qt6.7.2-Windows-x86_64-VS2022-17.10.3-20240621.7z
   else
     # Download Qt 6.7.2 for Windows ARM64
     echo "QTARM64: 6.7.2"
-    curl -LSO https://github.com/troffeelituf/nekoray/releases/download/qt-runtime/Qt6.7.2-Windows-arm64-VS2022-17.10.3-20240621.7z
+    # curl -LSO https://github.com/troffeelituf/nekoray/releases/download/qt-runtime/Qt6.7.2-Windows-arm64-VS2022-17.10.3-20240621.7z
+    curl -LSO https://github.com/MatsuriDayo/nekoray_qt_runtime/releases/download/20220503/Qt6.7.2-Windows-x86_64-VS2022-17.10.3-20240621.7z
   fi
 else
   if [ "$DL_QT_VER" == "5.15" ]; then
